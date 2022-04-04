47f75904f49cfb3202ea100b6036a6862412e9c9
joaquinrapetti <j.rapetti92@gmail.com>
2022-04-04 12:53:50 -0300

    course app

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = ({ title }) => {
+  return (
+    <header>
+        {title}
+    </header>);
+};
+
+export default Header;
---
+++ src/components/Item.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const Item = ({ item, handleCheck, handleDelete}) => {
+  return (
+    <li className="item" key={item.id}>
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={() => handleCheck(item.id)}
+      />
+      <label
+        htmlFor=""
+        onDoubleClick={() => handleCheck(item.id)}
+        style={item.checked ? { textDecoration: "line-through" } : null}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        role="button"
+        tabIndex="0"
+        onClick={() => handleDelete(item.id)}
+      />
+    </li>
+  );
+};
+
+export default Item;
